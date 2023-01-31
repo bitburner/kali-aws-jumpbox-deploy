b9e4bbb3838beeac502932f2d8e5bc4799318779
BitBurner <bitburner@users.noreply.github.com>
2023-01-31 11:53:22 -0800

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -49,4 +49,4 @@ echo "⚡ Installing tool s3sec to test AWS S3 buckets for read/write/delete acc
 sudo git clone https://github.com/0xmoot/s3sec
 echo "✅ Done installing s3sec"
 echo ""
-bash /home/kali/interact.sh
+bash interact.sh
